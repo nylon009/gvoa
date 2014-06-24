5bb0333530fced9fd3cc71bcead2dd041eeb6a81
nevinguo <nevinguo@gmail.com>
2014-06-24 23:22:31 +0800

    app version 1.0.6, show appversion in settings

--- src/com/gapp/gvoa/ui/GVOASettings.java
+++ src/com/gapp/gvoa/ui/GVOASettings.java
@@ -1,7 +1,9 @@
 package com.gapp.gvoa.ui;
 
 
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 import com.gapp.gvoa.R;
@@ -14,5 +16,21 @@ public class GVOASettings extends PreferenceActivity {
         
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        
+        Preference preference = findPreference("pref_about");
+        try {
+			String versionName = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+			preference.setSummary("GVOA Version: "+versionName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+         
+        
     }
+    
+    
+    
+    
 }
